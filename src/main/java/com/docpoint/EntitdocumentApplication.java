d14e5068c078acbafef8440dce4a352226aaef79
Kimhannah <skqkdldhf98@naver.com>
2024-04-09 11:29:15 +0900

    chore: initial commit

---
+++ src/main/java/com/docpoint/EntitdocumentApplication.java
@@ -0,0 +1,13 @@
+package com.docpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitdocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntitdocumentApplication.class, args);
+	}
+
+}
