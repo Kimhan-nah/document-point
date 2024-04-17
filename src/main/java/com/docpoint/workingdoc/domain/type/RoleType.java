69f4befafed40a54089d24da3740ce077211a2b9
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-04-17 10:44:17 +0900

    [Feat]: 기존 모듈의 enum class 추가 RoleType, WorkingStatusType, WorkingCategoryType 추가

---
+++ src/main/java/com/docpoint/workingdoc/domain/type/RoleType.java
@@ -0,0 +1,5 @@
+package com.docpoint.workingdoc.domain.type;
+
+public enum RoleType {
+    TEAM_LEADER, PART_LEADER, TEAM_MEMBER
+}
---
+++ src/main/java/com/docpoint/workingdoc/domain/type/WorkingCategoryType.java
@@ -0,0 +1,5 @@
+package com.docpoint.workingdoc.domain.type;
+
+public enum WorkingCategoryType {
+	OTHER, DEV
+}
