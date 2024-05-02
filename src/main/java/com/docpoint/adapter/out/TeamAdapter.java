2a68c0deae82caae3455808d8224251852c96667
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-05-02 19:48:52 +0900

    [Refactor]: TeamAdapter의 exception 수정

--- src/main/java/com/docpoint/adapter/out/TeamAdapter.java
+++ src/main/java/com/docpoint/adapter/out/TeamAdapter.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.docpoint.application.port.out.LoadTeamPort;
 import com.docpoint.common.annotation.PersistenceAdapter;
+import com.docpoint.common.exception.ErrorType;
 import com.docpoint.common.exception.custom.NotFoundException;
 import com.docpoint.domain.entity.Team;
 import com.docpoint.infrastructure.entity.TeamJpaEntity;
@@ -11,14 +12,15 @@ import com.docpoint.infrastructure.repository.TeamRepository;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @PersistenceAdapter
+@RequiredArgsConstructor
 public class TeamAdapter implements LoadTeamPort {
 	private final TeamRepository teamRepository;
 
 	@Override
 	public Optional<Team> load(long teamId) {
-		TeamJpaEntity team = teamRepository.findById(teamId).orElseThrow(NotFoundException::new);
+		TeamJpaEntity team = teamRepository.findById(teamId)
+			.orElseThrow(() -> new NotFoundException(ErrorType.NOT_FOUND_TEAM));
 		return Optional.of(TeamMapper.mapToDomainEntity(team));
 	}
 }
