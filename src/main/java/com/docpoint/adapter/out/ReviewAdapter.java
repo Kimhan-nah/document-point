8892ac8591da9f9d0db018a789ebfa5058c7d3af
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-05-06 00:04:21 +0900

    [Feat]: SaveReviewPort 구현체 Adapter 추가

--- src/main/java/com/docpoint/adapter/out/ReviewAdapter.java
+++ src/main/java/com/docpoint/adapter/out/ReviewAdapter.java
@@ -5,16 +5,18 @@ import java.util.Optional;
 
 import com.docpoint.adapter.out.mapper.ReviewMapper;
 import com.docpoint.application.port.out.LoadReviewPort;
+import com.docpoint.application.port.out.SaveReviewPort;
 import com.docpoint.common.annotation.PersistenceAdapter;
 import com.docpoint.domain.entity.Review;
 import com.docpoint.domain.entity.WorkingDocument;
+import com.docpoint.infrastructure.entity.ReviewJpaEntity;
 import com.docpoint.infrastructure.repository.ReviewRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
-public class ReviewAdapter implements LoadReviewPort {
+public class ReviewAdapter implements LoadReviewPort, SaveReviewPort {
 	private final ReviewRepository reviewRepository;
 
 	@Override
@@ -29,5 +31,11 @@ public class ReviewAdapter implements LoadReviewPort {
 			.stream()
 			.map(ReviewMapper::mapToDomainEntity)
 			.toList();
+	}
+
+	@Override
+	public Review save(Review review) {
+		ReviewJpaEntity reviewJpaEntity = ReviewMapper.mapToJpaEntity(review);
+		return ReviewMapper.mapToDomainEntity(reviewRepository.save(reviewJpaEntity));
 	}
 }
