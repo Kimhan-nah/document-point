86dd64822eb2319e0d0ca417022062d3106d7bcb
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-05-05 20:56:17 +0900

    [Feat]: SaveCpEvaluationPort 구현체 Adapter 구현 가###############

---
+++ src/main/java/com/docpoint/adapter/out/SaveCpEvaluationAdapter.java
@@ -0,0 +1,22 @@
+package com.docpoint.adapter.out;
+
+import com.docpoint.adapter.out.mapper.CpEvaluationMapper;
+import com.docpoint.application.port.out.SaveCpEvaluationPort;
+import com.docpoint.common.annotation.PersistenceAdapter;
+import com.docpoint.domain.entity.CpEvaluation;
+import com.docpoint.infrastructure.entity.CpEvaluationJpaEntity;
+import com.docpoint.infrastructure.repository.CpEvaluationRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class SaveCpEvaluationAdapter implements SaveCpEvaluationPort {
+	private final CpEvaluationRepository cpEvaluationRepository;
+
+	@Override
+	public CpEvaluation save(CpEvaluation cpEvaluation) {
+		CpEvaluationJpaEntity cpEvaluationJpaEntity = CpEvaluationMapper.mapToJpaEntity(cpEvaluation);
+		return CpEvaluationMapper.mapToDomainEntity(cpEvaluationRepository.save(cpEvaluationJpaEntity));
+	}
+}
