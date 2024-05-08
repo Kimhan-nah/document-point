e17d620b6c1c5f97c63307933f059a8c2adc9921
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-05-08 15:25:40 +0900

    [Feat]: SaveDocumentReviewerPort 구현 클래스 Adapter 생성

---
+++ src/main/java/com/docpoint/adapter/out/SaveDocumentReviewerAdapter.java
@@ -0,0 +1,22 @@
+package com.docpoint.adapter.out;
+
+import com.docpoint.adapter.out.mapper.DocumentReviewerMapper;
+import com.docpoint.application.port.out.SaveDocumentReviewerPort;
+import com.docpoint.common.annotation.PersistenceAdapter;
+import com.docpoint.domain.entity.DocumentReviewer;
+import com.docpoint.infrastructure.entity.DocumentReviewerJpaEntity;
+import com.docpoint.infrastructure.repository.DocumentReviewerRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class SaveDocumentReviewerAdapter implements SaveDocumentReviewerPort {
+	private final DocumentReviewerRepository documentReviewerRepository;
+
+	@Override
+	public DocumentReviewer save(DocumentReviewer documentReviewer) {
+		DocumentReviewerJpaEntity documentReviewerJpaEntity = DocumentReviewerMapper.mapToJpaEntity(documentReviewer);
+		return DocumentReviewerMapper.mapToDomainEntity(documentReviewerRepository.save(documentReviewerJpaEntity));
+	}
+}
