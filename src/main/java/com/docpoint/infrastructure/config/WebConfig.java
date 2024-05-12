323cd7a8aa9493373e21a181e728cb99e16f44b8
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-05-12 15:37:07 +0900

    [Fix]: 페이지네이션 1부터 시작하도록 수정

--- src/main/java/com/docpoint/infrastructure/config/WebConfig.java
+++ src/main/java/com/docpoint/infrastructure/config/WebConfig.java
@@ -2,7 +2,9 @@ package com.docpoint.infrastructure.config;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,4 +29,13 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(loginUserArgumentResolver);
 	}
+
+	@Bean
+	public PageableHandlerMethodArgumentResolverCustomizer customize() {
+		return p -> {
+			p.setOneIndexedParameters(true);    // 1부터 시작
+			p.setMaxPageSize(10);                // size=10
+		};
+	}
+
 }
