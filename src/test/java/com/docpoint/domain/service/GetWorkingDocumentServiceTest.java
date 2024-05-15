9b8c8c23cd2324cb7cfbe26b30ab37c01ecdd9e1
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-05-15 11:06:58 +0900

    [Test]: WorkingDocument, reviewer 조회 테스트 코드 작성 - 리뷰어 조회시 팀 리더가 필터링 되는지 검증하는 테스트 추가

--- src/test/java/com/docpoint/domain/service/GetWorkingDocumentServiceTest.java
+++ src/test/java/com/docpoint/domain/service/GetWorkingDocumentServiceTest.java
@@ -3,6 +3,7 @@ package com.docpoint.domain.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +14,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.docpoint.application.port.out.LoadWorkingDocumentPort;
+import com.docpoint.common.exception.ErrorType;
 import com.docpoint.common.exception.custom.NotFoundException;
+import com.docpoint.domain.entity.Team;
+import com.docpoint.domain.entity.User;
 import com.docpoint.domain.entity.WorkingDocument;
+import com.docpoint.domain.type.RoleType;
+import com.docpoint.util.UserTestData;
 import com.docpoint.util.WorkingDocumentTestData;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,8 +42,6 @@ class GetWorkingDocumentServiceTest {
 
 		// when
 		getWorkingDocumentService.getWorkingDocument(workingDocumentId);
-
-		// then
 	}
 
 	@Test
@@ -48,7 +52,42 @@ class GetWorkingDocumentServiceTest {
 
 		// when, then
 		assertThatThrownBy(() -> getWorkingDocumentService.getWorkingDocument(workingDocumentId))
-			.isInstanceOf(NotFoundException.class);
+			.isInstanceOf(NotFoundException.class)
+			.hasMessage(ErrorType.NOT_FOUND_WORKING_DOCUMENT.getMessage());
 	}
 
+	@Test
+	@DisplayName("삭제된 WorkingDocument일 경우, NotFoundException 발생")
+	void 삭제된_WorkingDocument일_경우() {
+		// given
+		long workingDocumentId = 1L;
+		WorkingDocument workingDocument = WorkingDocumentTestData.createWorkingDocument();
+		workingDocument.delete();
+
+		given(loadWorkingDocumentPort.loadById(workingDocumentId)).willReturn(Optional.of(workingDocument));
+
+		// when, then
+		assertThatThrownBy(() -> getWorkingDocumentService.getWorkingDocument(workingDocumentId))
+			.isInstanceOf(NotFoundException.class)
+			.hasMessage(ErrorType.DELETED_WORKING_DOCUMENT.getMessage());
+	}
+
+	@Test
+	@DisplayName("DocumentReviewer 목록 조회 성공 - TEAM_LEADER는 제외")
+	void getDocumentReviewers() {
+		// given
+		WorkingDocument workingDocument = WorkingDocumentTestData.createWorkingDocument();
+		User teamLeader = UserTestData.createTeamLeader(mock(Team.class));
+		User partLeader = UserTestData.createPartLeader(mock(Team.class));
+		User teamMember = UserTestData.createTeamMember(mock(Team.class));
+		List<User> reviewers = List.of(teamLeader, partLeader, teamMember);
+		given(loadWorkingDocumentPort.loadReviewers(workingDocument)).willReturn(reviewers);
+
+		// when
+		var documentReviewers = getWorkingDocumentService.getDocumentReviewers(workingDocument);
+
+		// then
+		assertThat(documentReviewers).hasSize(2);
+		assertThat(documentReviewers).allMatch(user -> user.getRole() != RoleType.TEAM_LEADER);
+	}
 }
