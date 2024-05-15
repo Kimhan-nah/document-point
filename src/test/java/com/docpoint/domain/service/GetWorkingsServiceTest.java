c78825c0fffa1ed08e30286616b8aca75ce58e73
skqkdldhf98 <skqkdldhf98@gmail.com>
2024-05-15 11:10:21 +0900

    [Test]: Working 조회 테스트 코드 작성

--- src/test/java/com/docpoint/domain/service/GetWorkingsServiceTest.java
+++ src/test/java/com/docpoint/domain/service/GetWorkingsServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,10 +14,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.docpoint.application.port.out.LoadUserWorkingPort;
+import com.docpoint.common.exception.ErrorType;
+import com.docpoint.common.exception.custom.NotFoundException;
 import com.docpoint.domain.entity.Team;
 import com.docpoint.domain.entity.User;
 import com.docpoint.domain.entity.Working;
 import com.docpoint.util.UserTestData;
+import com.docpoint.util.WorkingTestData;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("워킹 목록 조회 테스트")
@@ -37,6 +41,38 @@ class GetWorkingsServiceTest {
 		List<Working> workings = getWorkingsService.getWorkingsByTitle(user, "검색어");
 
 		assertThat(workings).isEmpty();
+	}
+
+	@Test
+	@DisplayName("Working이 없는 경우, NotFoundException 발생")
+	void Working이_없는_경우() {
+		given(loadUserWorkingPort.load(anyLong())).willReturn(Optional.empty());
+
+		assertThatThrownBy(() -> getWorkingsService.getWorkingById(anyLong()))
+			.isInstanceOf(NotFoundException.class)
+			.hasMessage(ErrorType.NOT_FOUND_WORKING.getMessage());
+	}
+
+	@Test
+	@DisplayName("삭제된 Working인 경우, NotFoundException 발생")
+	void 삭제된_Working인_경우() {
+		Working working = WorkingTestData.createDeletedWorking();
+		given(loadUserWorkingPort.load(anyLong())).willReturn(Optional.of(working));
+
+		assertThatThrownBy(() -> getWorkingsService.getWorkingById(anyLong()))
+			.isInstanceOf(NotFoundException.class)
+			.hasMessage(ErrorType.DELETED_WORKING.getMessage());
+	}
+
+	@Test
+	@DisplayName("Working 조회 성공")
+	void Working_조회_성공() {
+		Working working = WorkingTestData.createWorking();
+		given(loadUserWorkingPort.load(anyLong())).willReturn(Optional.of(working));
+
+		Working result = getWorkingsService.getWorkingById(anyLong());
+
+		assertThat(result).isEqualTo(working);
 	}
 
 }
